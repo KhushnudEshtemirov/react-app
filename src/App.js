980a11bc7c51b49776467fd23b639810183379e1
khushnudeshtemirov <khushnudeshtemirov@gmail.com>
2023-11-16 11:27:31 +0500

    Created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import log from "loglevel";
+
+import "./App.css";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    log.error("This is error message");
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Khushnud</h1>
     </div>
   );
 }
